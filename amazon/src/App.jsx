b7840ccd618cd1c98d5880eaf76cfe8a35359f41
Jayaraman-D <mr.jairam.d@gmail.com>
2025-07-11 08:51:15 +0530

    in app component render the images in db.json

--- amazon/src/App.jsx
+++ amazon/src/App.jsx
@@ -1,9 +1,49 @@
+import { useEffect, useState } from "react"
 
 
 function App() {
-  return(
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState('');
+  useEffect(() => {
+    fetch('http://localhost:3000/products')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('URL DOES NOT EXIST')
+        }
+        else {
+          return response.json()
+        }
+      })
+      .then((data) => {
+        setProducts(data)
+      })
+      .catch((error) => {
+        console.log(error)
+        setError(error.message)
+      })
+  }, [])
+
+  return (
     <div>
-      app component loaded
+      {
+
+        products.length > 0 ? (
+          products.map((product) => (
+            <div key={product.id}>
+              <h2>{product.name}</h2>
+              <img src={product.image} alt={product.name} />
+            </div>
+          )))
+
+
+          : (
+            <div>
+              products loading
+            </div>
+          )}
+
+
+
     </div>
   )
 }
