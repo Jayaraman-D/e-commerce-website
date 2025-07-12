41a8f6157852b1d3093dc750b4b04ac065bd3b95
Jayaraman-D <mr.jairam.d@gmail.com>
2025-07-12 20:18:20 +0530

    created Nav and Products components

--- amazon/src/App.jsx
+++ amazon/src/App.jsx
@@ -1,50 +1,19 @@
-import { useEffect, useState } from "react"
+// App.jsx File
 
 
-function App() {
-  const [products, setProducts] = useState([]);
-  const [error, setError] = useState('');
-  useEffect(() => {
-    fetch('http://localhost:3000/products')
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('URL DOES NOT EXIST')
-        }
-        else {
-          return response.json()
-        }
-      })
-      .then((data) => {
-        setProducts(data)
-      })
-      .catch((error) => {
-        console.log(error)
-        setError(error.message)
-      })
-  }, [])
-
-  return (
-    <div>
-      {
-
-        products.length > 0 ? (
-          products.map((product) => (
-            <div key={product.id}>
-              <h2>{product.name}</h2>
-              <img src={product.image} alt={product.name} />
-            </div>
-          )))
-
-
-          : (
-            <div>
-              products loading
-            </div>
-          )}
+import React from 'react'
+import Navbar from './Navbar'
+import Products from './Products'
 
 
 
-    </div>
+
+function App (){
+  return(
+    <>
+      <Navbar/>
+      <Products />
+    </>
   )
 }
 
---
+++ amazon/src/Products.jsx
@@ -0,0 +1,11 @@
+// Products.jsx File
+
+import React from 'react'
+
+function Products() {
+  return (
+    <div className='products'>Products Component</div>
+  )
+}
+
+export default Products
