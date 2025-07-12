1eb4c493692103dbf6b890fe82b1b6150aa7b973
Jayaraman-D <mr.jairam.d@gmail.com>
2025-07-12 21:45:20 +0530

    navbar section completed

--- amazon/src/Navbar.jsx
+++ amazon/src/Navbar.jsx
@@ -4,13 +4,25 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='nav'>
-  <div className="amazon-logo">
-    <img src='public/logos/amazon-logo-white.png' alt='logo' />
-  </div>
-  
+    <div className='navs'>
+      <div className="amazon-logo">
+        <img src='public/logos/amazon-logo-white.png' alt='logo' />
+      </div>
 
+      <div className="search">
+        <input type='text' placeholder='search' />
+        <i className="bi bi-search"></i>
+      </div>
 
+      <div className="returns-orders">
+        Returs & Orders
+      </div>
+
+      <div className="cart">
+        cart
+        <i className="bi bi-cart3"></i>
+        <span>0</span>
+      </div>
 
     </div>
   )
